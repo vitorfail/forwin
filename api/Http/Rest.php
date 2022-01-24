96be5b48063370c08c48368dbc627176f44b2258
vitorfail <95591747+vitorfail@users.noreply.github.com>
2022-01-23 23:55:57 -0300

    Change

--- api/Http/Rest.php
+++ api/Http/Rest.php
@@ -1,5 +1,6 @@
 <?php
     namespace Map\Http;
+    require_once('Controllers/UsersController.php');
 
     class Rest{
         private $request;
@@ -7,17 +8,15 @@
         private $method;
         private $params = array();
 
-        public function __constructor($req){
+        public function __construct($req){
             $this->request = $req;
             $this->load();
         }
         public function load(){
             $newURL = explode('/', $this->request['url']);
-            array_shift($newURL); 
             if(isset($newURL[0])){
                 $this->class = ucfirst($newURL[0]).'Controller';
                 array_shift($newURL);
-                return ucfirst($newURL).'Controller';
                 if(isset($newURL[0])){
                     $this->method = $newURL[0];
                     array_shift($newURL);
@@ -28,7 +27,7 @@
             }
         }
         public function run(){
-            if(class_exists('\Map\Http\Controllers\\'.$this->class) && method_exists('\Map\Http\Controllers\\'. $this->class, $this->method)){
+            if(class_exists('\Map\Http\Controllers\\'.$this->class) && method_exists('\Map\Http\Controllers\\'.$this->class, $this->method)){
                 try{
                     $controll = '\Map\Http\Controllers\\'.$this->class;
                     $response = call_user_func_array(array(new $controll, $this->method), $this->params);
@@ -39,7 +38,7 @@
                 }
             }
             else{
-                return json_encode(array('data' => 'Operação inválida', 'status' => 'error'), JSON_UNESCAPED_UNICODE);
+                return json_encode(array('data' => $this->class, 'status' => $this->method), JSON_UNESCAPED_UNICODE);
             }
         }
     }
--- api/index.php
+++ api/index.php
@@ -1,24 +1,9 @@
 <?php
     require_once('Http/Rest.php');
-    require_once('Http/Controllers/UsersController.php');
     use Map\Http\Rest;
 
     if(isset($_GET) && !empty($_GET)){
-        //$rest = new Rest($_REQUEST);
-        //echo $rest->run();
-        $req = explode('/', $_GET['url']);
-        $class = ucfirst($req[0]).'Controller';
-        array_shift($req);
-        if(class_exists('Map\Http\Controllers\\'.$class)){
-            $method = $req[0];
-            if(method_exists('Map\Http\Controllers\\'.$class, $method)){
-                echo var_dump($_GET);
-            }
-            else{
-                echo 'Map\Http\Controllers\\'.'\\'.$class.'\\'.$method;
-            }
-        }
-        else{
-        }
+        $rest = new Rest($_GET);
+        echo $rest->run();
     }
 ?>
