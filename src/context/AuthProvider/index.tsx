299dffe136d2f590d81f83d6ce3a8879ff9d7b7f
vitorfail <vitor_andrademanoel@hotmail.com>
2022-01-31 22:12:38 -0300

    Change

--- src/context/AuthProvider/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from "react";
-import { IContext } from "./types";
-
-export const Authcontext = createContext<IContext>({} as IContext);
-export const AuthProvider = ()
