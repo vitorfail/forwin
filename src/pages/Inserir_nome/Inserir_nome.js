488f267b5fc0cd794860df182adb72fccd07d420
vitorfail <95591747+vitorfail@users.noreply.github.com>
2022-02-01 16:59:59 -0300

    Change

--- src/pages/Inserir_nome/Inserir_nome.js
+++ src/pages/Inserir_nome/Inserir_nome.js
@@ -10,6 +10,17 @@ function Inserir_nome(props){
     const [tema_roxo, setTema_roxo] = useState(false);
     const [tema_laranja, setTema_laranja] = useState(false);
     const [nome, setNome] = useState('');
+
+    function mudar_conta(){
+        const Axios = axios.create({
+            baseURL:apis
+        })
+        Axios.post('index.php?url=nome/pesquisa', )
+    }
+    function troca_de_tema(tema){
+        var html = document.querySelector('html');
+        html.className = tema;
+    }
     return(
         <div className={props.mostrar}>
             <div className='modal'>
@@ -17,10 +28,10 @@ function Inserir_nome(props){
                     <h1 className='nickname'>{nome}</h1>
                     <input className='mudar-nome' type='text'/>
                     <div className='escolha-tema'>
-                        <input type='radio' name='tema' checked={true} id='tema-padrao' value='tema-padrao' onClick='troca_de_tema(this.value)'/><h2 className='padrao'>11</h2><p>Tema padrao</p>
-                        <input type='radio' name='tema' checked={false} id='tema-azul' value='tema-azul' onClick='troca_de_tema(this.value)'/><h2 className='azul'>11</h2><p>Tema azul</p>
-                        <input type='radio' name='tema' checked={false} id='tema-roxo' value='tema-roxo' onClick='troca_de_tema(this.value)'/><h2 className='roxo'>11</h2><p>Tema roxo</p>
-                        <input type='radio' name='tema' checked={false} id='tema-laranja'value='tema-laranja' onClick='troca_de_tema(this.value)'/><h2 className='laranja'>11</h2><p>Tema laranja</p>
+                        <input type='radio' name='tema'  id='tema-padrao' value='tema-padrao' onClick={(event) => troca_de_tema(event.target.value)}/><h2 className='padrao'>11</h2><p>Tema padrao</p>
+                        <input type='radio' name='tema' id='tema-azul' value='tema-azul' onClick={(event) => troca_de_tema(event.target.value)}/><h2 className='azul'>11</h2><p>Tema azul</p>
+                        <input type='radio' name='tema' id='tema-roxo' value='tema-roxo' onClick={(event) => troca_de_tema(event.target.value)}/><h2 className='roxo'>11</h2><p>Tema roxo</p>
+                        <input type='radio' name='tema' id='tema-laranja'value='tema-laranja' onClick={(event) => troca_de_tema(event.target.value)}/><h2 className='laranja'>11</h2><p>Tema laranja</p>
                     </div>
                     <div className='menu_nome'>
                         <button className='mudar'>Mudar</button>
