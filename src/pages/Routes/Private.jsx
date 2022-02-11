5d7c334d26982e67cdaf012c1da238e27062772a
vitorfail <95591747+vitorfail@users.noreply.github.com>
2022-02-11 19:20:31 -0300

    Change

--- src/pages/Routes/Private.jsx
+++ src/pages/Routes/Private.jsx
@@ -1,34 +1,33 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { Navigate, Outlet} from 'react-router-dom';
 import StoreContext from '../Store/Context';
 import axios from 'axios';
 import { apis } from '../../caminho_api.mjs';
 
+async function Query(){
+    const token = localStorage.getItem('token_jwt')
+    const Axios = axios.create({
+        baseURL:apis
+    })
+    const resolve = await Axios.post('index.php?url=auth/checkAuth', {}, {headers: {"Authorization": "Bearer "+ token}})
+    return resolve.data
+}
+
 const RoutesPrivate = () => {
-    const {token} = useContext(StoreContext);
-    async function query(){
-        var valid = false
-        const Axios = axios.create({
-            baseURL:apis
-        })
-        await Axios.post('index.php?url=auth/checkAuth', {}, {headers: {"Authorization": "Bearer "+ token}})
-        .then(res =>{
-            if(res.data.data === 'Operação inválida' || res.data.data === false){
-                valid = false
-            }
-            if(res.data.data === true){
-                valid = true
-            }
-            else{
-                valid = false
-            }
-        })
-        .catch(er =>{
-            valid =false
-        })
-        return valid
+    const [valid, setvalid] = useState(false)
+    async function Resolver(){
+        const resolve = await Query();
+        setvalid(resolve.data)
+
+        var dados = resolve.data
+        if (dados === "Operação inválida" || dados === false){
+        }
+        if(dados === true){
+        }
+        else{
+        }
     }
-    console.log(query())
-    return query()? <Outlet/> : <Navigate to='/login'/>
+    Resolver()
+    return valid? <Outlet/> : <Navigate to='/login'/>
 }
 export default RoutesPrivate;
