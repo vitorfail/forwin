e8f12aa7ad564f0b00cd68fdd96275c61a1d235c
vitorfail <vitor_andrademanoel@hotmail.com>
2022-02-15 12:01:30 -0300

    Funciona

--- src/routes.js
+++ src/routes.js
@@ -6,23 +6,21 @@ import Financeiro from './pages/Financeiro';
 import Pagamentos from'./pages/Pagamentos';
 import Login from './pages/Login/Login';
 import Home from './pages/Home';
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import StoreProvider from './pages/Store/Provider';
 import RoutesPrivate from "./pages/Routes/Private";
-import { Switch } from "react-router-dom";
-
 
 const Rout = () => (
   <BrowserRouter>
     <StoreProvider>
       <Switch>
         <Route  exact path="/login" component={Login}/>
-        <Route  exact path="/" component={Home}/>
+        <RoutesPrivate  exact path="/" component={Home}/>
         <RoutesPrivate  exact path="/cadastro" component={Cadastro}/>
-        <Route  exact path="/graficos" component={Graficos}/>
-        <Route  exact path="/financeiro" component={Financeiro}/>
-        <Route  exact path="/pagamentos" component={Pagamentos}/>
-        <Route  path="/pesquisa/:nome" component={Pesquisa}/>
+        <RoutesPrivate  exact path="/graficos" component={Graficos}/>
+        <RoutesPrivate  exact path="/financeiro" component={Financeiro}/>
+        <RoutesPrivate  exact path="/pagamentos" component={Pagamentos}/>
+        <RoutesPrivate  path="/pesquisa/:nome" component={Pesquisa}/>
       </Switch>
     </StoreProvider>
   </ BrowserRouter>
