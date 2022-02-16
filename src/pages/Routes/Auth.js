f09d35f2997270666396efa074ce61e910c8e96c
vitorfail <vitor_andrademanoel@hotmail.com>
2022-02-16 13:11:41 -0300

    Pal

--- src/pages/Routes/Auth.js
+++ src/pages/Routes/Auth.js
@@ -3,31 +3,21 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import { apis } from '../../caminho_api.mjs';
 
-function Resolver(){
-    const token = localStorage.getItem('token_jwt')
-    const [v, setv] = useState(false)
-    var valid = null
-    async function parseJwt(token) {
-        try{
-            if(token != null){
-                const Axios = axios.create({
-                    baseURL:apis
-                })
-                Axios.post('index.php?url=auth/checkAuth', {}, {headers: {"Authorization": "Bearer "+ token}})
-                .then(res => {
-                    valid = res.data.data
-                })
-
-            }
-            else{
-                valid = null
-            }
+async function Resolver() {
+    try{
+        const token = localStorage.getItem('token_jwt')
+        if(token != null){
+            const Axios = axios.create({
+                baseURL:apis
+            })
+            return Axios.post('index.php?url=auth/checkAuth', {}, {headers: {"Authorization": "Bearer "+ token}})
         }
-        catch{
-            valid = null
-        }  
+        else{
+            return null
+        }
     }
-    parseJwt(token)
-    return valid
+    catch{
+        return null
+    }  
 }
 export default Resolver()
--- src/pages/Routes/Private.jsx
+++ src/pages/Routes/Private.jsx
@@ -1,14 +1,26 @@
-import React, {useCallback, useContext, useEffect, useState} from 'react';
+import { useState } from 'react';
 import { Redirect, Route} from 'react-router-dom';
 import StoreContext from '../Store/Context';
 import Resolver from './Auth'
 
+const L = () =>{
+    Resolver.then(res => {
+        if(res === null){
+            return null
+        }
+        else{
+            console.log(res.data.data)
+            return res.data.data
+        }
+    })
+}
+
+
 const RoutesPrivate = ({component: Component, ...rest}) => {
 
-    console.log(Resolver)
     return (<Route 
                 {...rest} 
-                render={( ) => Resolver?
+                render={( ) => L()?
                      <Component {...rest}/> : 
                      <Redirect to='/login'/>}/>)
 }
