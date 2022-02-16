ae5dd22b25306eece8901501a30ad2386680d2d0
vitorfail <95591747+vitorfail@users.noreply.github.com>
2022-02-16 19:46:48 -0300

    Jub

--- src/pages/Routes/Private.jsx
+++ src/pages/Routes/Private.jsx
@@ -1,27 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Redirect, Route} from 'react-router-dom';
 import StoreContext from '../Store/Context';
 import Resolver from './Auth'
 
-const L = () =>{
-    Resolver.then(res => {
-        if(res === null){
-            return null
-        }
-        else{
-            console.log(res.data.data)
-            return res.data.data
-        }
-    })
-}
-
-
 const RoutesPrivate = ({component: Component, ...rest}) => {
-
-    return (<Route 
+    const [v, setv] = useState(false)
+    useEffect(() => {
+        Resolver.then(res => {
+            if(res === null){
+                setv(null)
+            }
+            else{
+                console.log(res.data.data)
+                setv(true)
+            }
+        })
+        return () => {
+            setv(false)
+        }
+    },[])
+    return (
+            <Route 
                 {...rest} 
-                render={( ) => L()?
-                     <Component {...rest}/> : 
-                     <Redirect to='/login'/>}/>)
+                render={( ) => v?
+                     (<Component {...rest}/>) : 
+                     (<Redirect to='/login'/>)}/>)
 }
 export default RoutesPrivate
