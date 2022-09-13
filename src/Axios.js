f31d579e8945961730ca9df5f8465a3982e96798
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-13 12:13:01 -0300

    1234

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: 'http://localhost/public_html/mysql_con'
+  baseURL: 'http://localhost/mysql_con'
 });
 
 Axios.interceptors.request.use(async config => {
